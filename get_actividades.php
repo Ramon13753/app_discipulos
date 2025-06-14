5d6a7a2d85f7ccbd90192a3fbea97003888a4d23
Ramon13753 <hincapieramonelias@gmail.com>
2025-06-14 13:34:44 -0500

    Update get_actividades.php

--- get_actividades.php
+++ get_actividades.php
@@ -15,11 +15,11 @@ set_error_handler(function($errno, $errstr, $errfile, $errline) {
 });
 
 // *** ASEGÚRATE DE USAR LAS MISMAS CREDENCIALES DE BD QUE TE FUNCIONARON ***
-$servername = "mysql-5v2t-production.up.railway.app";
+$servername = "shuttle.proxy.rlwy.net";
 $username = "root"; // Usuario de tu base de datos
 $password = "NXcdHmwfHhmucKqdmxPCYMLrRFDMiyNu"; // Contraseña de tu base de datos
 $dbname = "discipulos_de_cristo"; // Nombre de tu base de datos
-$port = 33060; // Puerto de tu base de datos
+$port = 40395; // Puerto de tu base de datos
 
 $conn = new mysqli($servername, $username, $password, $dbname, $port);
 

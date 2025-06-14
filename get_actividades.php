b6896db72cdbf158d5fd8a69e5adbaf0c77818d8
Ramon13753 <hincapieramonelias@gmail.com>
2025-06-14 12:55:11 -0500

    Update get_actividades.php

--- get_actividades.php
+++ get_actividades.php
@@ -19,7 +19,7 @@ $servername = "mysql-5v2t-production.up.railway.app";
 $username = "root"; // Usuario de tu base de datos
 $password = "NXcdHmwfHhmucKqdmxPCYMLrRFDMiyNu"; // Contraseña de tu base de datos
 $dbname = "discipulos_de_cristo"; // Nombre de tu base de datos
-$port = 33060; // Puerto de tu base de datos
+$port = 3306; // Puerto de tu base de datos
 
 $conn = new mysqli($servername, $username, $password, $dbname, $port);
 
